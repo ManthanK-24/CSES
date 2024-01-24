90df5ee56e5c51c71e663101771ac4d560a80db9
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-01-24 16:36:28 +0530

    Create trailingZeroes.cpp

---
+++ Introductory Problems/trailingZeroes.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef  long long int ll;
+
+
+int main()
+{
+#ifndef  ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+
+	int tc = 1;
+	//cin >> tc;
+	for (int ii = 1; ii <= tc; ii++)
+	{
+		//cout<<"Case #"<<ii<<": ";
+
+		ll n;
+		cin >> n;
+		ll mod = 1e9 + 7;
+
+		ll ans = 0, cnt = 5;
+
+		while (cnt <= n)
+		{
+			ans += n / cnt;
+			cnt *= 5;
+		}
+		cout << ans;
+
+
+		cout << "\n";
+
+	}
+
+	return 0;
+}
